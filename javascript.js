aa7e380101b4a2e7f6184c3e685ddc3ec66c4b5d
Hunter Yeago <hyradar@tutamail.com>
2022-09-23 19:06:13 +1000

    added box shadow, form functionality

---
+++ javascript.js
@@ -0,0 +1,10 @@
+
+
+let ogpass = document.getElementById('ogpassword').innerText;
+let cfpass = document.getElementById('cfpassword').innerText;
+
+let submit = document.querySelector('.createaccount');
+
+if (ogpass != cfpass) {
+
+}
